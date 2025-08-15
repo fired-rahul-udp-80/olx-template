7de96fb38ec989821a134500e42412efdca6562f
Rahul Kumar <developerrahul977@gmail.com>
2025-08-16 01:40:43 +0530

    fouth

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
         <div className="flex-1">
           <input
             className="w-full rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Search"
+            placeholder="Search here"
           />
         </div>
         <Link
